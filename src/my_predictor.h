728bfa3cf159faaf9275a04aa9a537f737cbb086
reidoboyle <oboylere@tamu.edu>
2022-12-06 22:11:10 -0600

    final fixes

--- src/my_predictor.h
+++ src/my_predictor.h
@@ -2,6 +2,9 @@
 // This file contains a sample my_predictor class.
 // It has a simple 32,768-entry gshare with a history length of 15 and a
 // simple direct-mapped branch target buffer for indirect branch prediction.
+
+//https://github.com/taraeicher/HybridBranchPredictor source for choice table perceptron
+
 #include "predictors/tage/TAGEPredictor.h"
 #include "predictors/piecewise/piecewise_predictor.h"
 
@@ -21,18 +24,23 @@ public:
 	unsigned int targets[1<<TABLE_BITS];
 	int choiceTable[25000];
 	bitset<20> GHR;
-	int tage_or_pwl = 0; // tage used = 1, pwl used = 0
-	bool equalPredictions = 0;
-	PREDICTOR* tage = new PREDICTOR();
-	Piecewise* pwl = new Piecewise();
-	int tage_miss = 0;
-	int pwl_miss = 0;
-	int tage_preds = 0;
-	int pwl_preds = 0;
+	int tage_or_pwl; // tage used = 1, pwl used = 0
+	bool equalPredictions;
+	PREDICTOR* tage;
+	Piecewise* pwl;
+	int pwl_miss;
+	int tage_miss;
+
 	my_predictor (void) : history(0) { 
 		memset (tab, 0, sizeof (tab));
 		memset (targets, 0, sizeof (targets));
 		GHR = bitset<20>();
+		tage_or_pwl = 0;
+		tage = new PREDICTOR();
+		pwl = new Piecewise();
+		equalPredictions = 0;
+		pwl_miss = 0;
+		tage_miss = 0;
 		  // Initialize the choice table.
 	for(UINT32 iii=0; iii < 25000; iii++){
 		choiceTable[iii] = 0;
@@ -47,7 +55,9 @@ public:
 
 		if(b.br_flags & BR_CONDITIONAL)
 		{
-			
+			u.index = (history << (TABLE_BITS - HISTORY_LENGTH)) ^ (b.address & ((1<<TABLE_BITS)-1));
+            int gshare_predict = tab[u.index] >> 1;
+
 			bool tage_predict= tage->GetPrediction(b.address);
 
 			bool piecewise_predict = pwl->predict(b.address);
@@ -66,7 +76,12 @@ public:
 			else // predictions differ
 			{
 				int tableIdx = b.address % 25000;// HashPC(b.address,16000);
-				if(choiceTable[tableIdx] < 2 or piecewise_confidence < 100)
+				if(gshare_predict > 0 and piecewise_predict > 0 and abs(tage_predict) < 3)
+				{
+					tage_or_pwl = 0;
+					u.direction_prediction(piecewise_predict);
+				}
+				else if(choiceTable[tableIdx] < 2 or piecewise_confidence < 100)
 				{
 					tage_or_pwl = 1;
 					u.direction_prediction(tage_predict);
@@ -141,25 +156,43 @@ public:
 
 	void update (branch_update *u, bool taken, unsigned int target) {
 		if (bi.br_flags & BR_CONDITIONAL) {
-			GHR = (GHR<<1);
-			if(taken) {GHR.set(0,1);} else {GHR.set(0,0);}
+			//update gshare
+            unsigned char *c = &tab[((my_update*)u)->index];
+            if (taken) {if (*c < 3) (*c)++;} else {if (*c > 0) (*c)--;}
+                history <<= 1;
+                history |= taken;
+                history &= (1<<HISTORY_LENGTH)-1;
+
 			int tableIdx = bi.address % 25000;//HashPC(bi.address,16000);
 			if((taken != u->direction_prediction()) and choiceTable[tableIdx] < 2)
 			{
-				choiceTable[tableIdx] += 1;
+				if(choiceTable[tableIdx < 128])
+				{
+					choiceTable[tableIdx] += 1;
+				}
+				
 			}
 			else if((taken != u->direction_prediction()) and choiceTable[tableIdx] >= 2)
 			{
-				choiceTable[tableIdx] -= 1;
+				if(choiceTable[tableIdx > -128])
+				{
+					choiceTable[tableIdx] -= 1;
+				}
 			}
 			
 			if(taken == u->direction_prediction() and choiceTable[tableIdx] < 2)
 			{
-				choiceTable[tableIdx] -= 1;
+				if(choiceTable[tableIdx > -128])
+				{
+					choiceTable[tableIdx] -= 1;
+				}
 			}
 			else if (taken == u->direction_prediction() and choiceTable[tableIdx] >= 2)
 			{
-				choiceTable[tableIdx] += 1;
+				if(choiceTable[tableIdx < 128])
+				{
+					choiceTable[tableIdx] += 1;
+				}
 			}
 			/*if((choiceTable[tableIdx][1] == 0 and u->target_prediction() == taken) or (choiceTable[tableIdx][1] = 1 and taken != u->target_prediction()))
 			{
